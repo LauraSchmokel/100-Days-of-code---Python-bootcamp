5d7fa4bce805ebd78fa8d3bea732eb7ee016623b
Laura Schmokel <schmokel.laura@gmail.com>
2025-11-17 20:29:38 -0300

    Day 1

---
+++ Day_1.py
@@ -0,0 +1,37 @@
+"""
+1. print
+2. input
+3. concatenação usando o "+"
+4. variáveis
+5. comentários sando "#"
+6. Nova linha usando o "\n"
+
+
+"""
+#print("Hello World!")
+
+#print("Hello, " + "Laura!")
+
+#print("Hello, " + input("What is your name? ") + "!")
+
+# name = input("What is your name? ")
+# print(name)
+
+# name = "Jack"
+# print(name)
+
+# name = "Laura"
+# print(name)
+
+# print(len(input("What's your name?")))
+
+# username = input("What's your name? ")
+# lenght = len(username)
+# print(lenght)
+
+#CHALLENGE
+#Band name generator
+
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be:", city_name, pet_name)
