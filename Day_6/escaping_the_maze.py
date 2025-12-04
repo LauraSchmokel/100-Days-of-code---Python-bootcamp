cf9b26be638b950bce374cead664e3b283babfb6
Laura Schmokel <schmokel.laura@gmail.com>
2025-12-03 22:29:41 -0300

    Day 6

---
+++ Day_6/escaping_the_maze.py
@@ -0,0 +1,32 @@
+
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+
+# my_function()
+
+#THIS CODE WAS MADE IN REEBORG'S WORLD
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def turn_around():
+#     turn_left()
+#     turn_left()
+    
+# def wall_on_left():
+#     turn_left()
+#     result = front_is_clear()
+#     turn_right()
+#     return result
+    
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
