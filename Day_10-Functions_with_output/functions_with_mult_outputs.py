335df18608cddeb671d8e699f6cdfcfdb9e084f9
Laura Schmökel <schmokel.laura@gmail.com>
2026-01-12 20:10:18 -0300

    Day 10

---
+++ Day_10-Functions_with_output/functions_with_mult_outputs.py
@@ -0,0 +1,17 @@
+def format_name(f_name, l_name):
+    """
+    To check if the names are empty or not
+    """
+    if f_name == "" or l_name == "":
+        return f"There is an empty name!"
+
+    return f"{f_name.title()} {l_name.title()}"
+    
+
+f_name = input("Enter your first name: ")
+l_name = input("Enter your last name: ")
+
+titled_name = format_name(f_name=f_name, l_name=l_name)
+
+print(titled_name)
+
