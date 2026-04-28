e504498413a7bb155ca10bd9b6afe78dc1c55930
Laura Schmökel <schmokel.laura@gmail.com>
2026-04-28 19:36:17 -0300

    Day_16

---
+++ Day_16-OOP/oop_activity.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+from prettytable import PrettyTable
+
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("blue")
+# timmy.forward(100)
+
+# my_screen = Screen()
+# # print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Charmander", "Squirtle", "Bulbasaur"])
+table.add_column("Pokemon Type", ["Fire", "Water", "Plant"])
+
+# table.field_names = ["Pokemon Name", "Pokemon Type"]
+# table.add_row(["Charmander", "Fire"])
+# table.add_row(["Squirtle", "Water"])
+# table.add_row(["Bulbasaur", "Plant"])
+
+table.align = "l"
+
+print(table)
